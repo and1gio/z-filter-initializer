ff0c4d6496e494a9bdc0102c01257551451e6564
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2016-12-12 15:03:55 +0400

    critical bugfix - temporary solution

--- z-filter-initializer.js
+++ z-filter-initializer.js
@@ -7,18 +7,22 @@ module.exports = {
         var folderPath = app.folderPath.app.root + app.config.zFilter.rootDir + '/';
         var filters = app.config.zFilter.filters;
 
+        var modules = {};
+
         for (var i in filters) {
-            var module = require(folderPath + filters[i])(app);
-            app.filter[filters[i]] = {};
+            app.filter[filters[i]] = require(folderPath + filters[i])(app);
+
+            modules[filters[i]] = require(folderPath + filters[i])(app);
+            var module = modules[filters[i]];
 
             for (var j in module) {
                 (function (indexI, indexJ) {
                     app.filter[indexI][indexJ] = function (data) {
                         return function (req, res, next) {
                             if (data) {
-                                module[indexJ](data, req, res, next);
+                                modules[indexI][indexJ](data, req, res, next);
                             } else {
-                                module[indexJ](req, res, next);
+                                modules[indexI][indexJ](req, res, next);
                             }
                         }
                     }
