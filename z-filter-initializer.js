01f092a134df638618b6dfbaafa85887ea476bf2
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2017-04-07 16:53:56 +0400

    bugfix

--- z-filter-initializer.js
+++ z-filter-initializer.js
@@ -7,28 +7,28 @@ module.exports = {
         var folderPath = app.folderPath.app.root + app.config.zFilter.rootDir + '/';
         var filters = app.config.zFilter.filters;
 
-        var modules = {};
-
         for (var i in filters) {
-            app.filter[filters[i]] = require(folderPath + filters[i])(app);
+            (function (indexI) {
+                var module = require(folderPath + filters[indexI])(app);
+                app.filter[filters[indexI]] = {};
 
-            modules[filters[i]] = require(folderPath + filters[i])(app);
-            var module = modules[filters[i]];
-
-            for (var j in module) {
-                (function (indexI, indexJ) {
-                    app.filter[indexI][indexJ] = function (data) {
-                        return function (req, res, next) {
-                            if (data) {
-                                modules[indexI][indexJ](data, req, res, next);
-                            } else {
-                                modules[indexI][indexJ](req, res, next);
+                for (var j in module) {
+                    (function (idxI, idxJ) {
+                        app.filter[idxI][idxJ] = function (data) {
+                            return function (req, res, next) {
+                                if (data) {
+                                    module[indexJ](data, req, res, next);
+                                } else {
+                                    console.log(idxI, idxJ, module);
+                                    module[idxJ](req, res, next);
+                                }
                             }
                         }
-                    }
-                })(filters[i], j);
-            }
+                    })(filters[indexI], j);
+                }
+            })(i)
         }
+
         next();
     }
 };
